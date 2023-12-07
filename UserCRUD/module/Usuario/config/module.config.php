3e09a7ef40d276f6072d7426f3cc135b55a398fb
Matheus Freitas <matheusalbuf@gmail.com>
2023-12-07 16:05:28 -0300

    Configurando o ambiente

---
+++ UserCRUD/module/Usuario/config/module.config.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace Usuario; 
+use Laminas\Router\Http\Segment;
+use Usuario\Controller\UsuarioController;
+use Laminas\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'router' => [
+        'routes' => [
+            'usuario' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/usuario[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'id' => '[0-9]+'],
+                    'defaults' => [
+                        'controller' => Controller\UsuarioController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class,
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [ 'usuario' => __DIR__ . '/../view',
+        ],
+    ],
+];
+
+
+
+
+
+?>
